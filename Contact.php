1b8913cdb22dbd6b2b73cae374de7d36443eab36
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-01-09 16:29:24 +0545

    Fix stylesheet path in Contact.php for correct loading of styles

--- Contact.php
+++ Contact.php
@@ -10,7 +10,7 @@ $userEmail = $isLoggedIn ? $_SESSION['email'] : '';
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ContactPage</title>
-    <link rel="stylesheet"href="contact.css"/>
+    <link rel="stylesheet"href="./css/contact.css"/>
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
