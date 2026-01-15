ffd85c3d99625a1e644a31b58356199163cc4173
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-01-15 20:49:52 +0545

    Update profile.php title to be more concise

--- Backend/profile.php
+++ Backend/profile.php
@@ -19,7 +19,7 @@ $userData = mysqli_fetch_assoc($result);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile - TravelLocal Nepal</title>
+    <title>User Profile</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         body {
