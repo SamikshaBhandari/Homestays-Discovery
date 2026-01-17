59f844c3a9be1ff5f548bb590e909471b96eb561
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-01-17 14:03:43 +0545

    Fix title formatting in confirm_booking.php for improved clarity

--- confirm_booking.php
+++ confirm_booking.php
@@ -47,7 +47,7 @@ $profile_image     = $homestay['profile_image'] ? 'images/' . $homestay['profile
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Complete Booking - <?php echo $homestay_name; ?></title>
+  <title>Complete Booking<?php echo $homestay_name; ?></title>
   <link rel="stylesheet" href="./css/confirm_booking.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
 </head>
