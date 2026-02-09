96a76cde04b68db54243635f35ff35902b5a99e0
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-02-09 20:34:15 +0545

    Refactor logout process: streamline session handling and remove alert script

--- Backend/logout.php
+++ Backend/logout.php
@@ -1,16 +1,8 @@
 <?php
 session_start();
-$_SESSION = array();
-
-if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time()-42000, '/');
-}
+session_unset();
 session_destroy();
-echo "
-<script>
-    alert('Logout successfully!');
-    window.location.href = '../index1.php';
-</script>
-";
-exit();
+setcookie("isLoggedIn", "", time() - 3600, "/");
+header("Location: ../index1.php");
+exit;
 ?>
